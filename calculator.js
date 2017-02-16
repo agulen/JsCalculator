601304a24d3f98fbfcc5894833bc383617a91e6c
Altan G <gulenak@gmail.com>
2017-02-15 21:50:24 -0800

    First commit. Sucking at something is the first step to being kinda good at something =)

---
+++ calculator.js
@@ -0,0 +1,28 @@
+function clearStatus() {
+	alert('clear');
+}	
+
+function append(num) {
+	alert(num);
+}
+
+
+function add() {
+
+}
+
+function sub() {
+
+}
+
+function mul() {
+
+}
+
+function div() {
+
+}
+
+function calculate() {
+
+}
